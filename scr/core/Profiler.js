15e473bdb6f4f4b1f2c1f1ba11fae8697f20b6a8
nullkommaneun <thom.ehr@gmail.com>
2025-08-25 19:57:47 +0200

    Delete scr/core directory

--- scr/core/Profiler.js
+++
@@ -1,5 +0,0 @@
-export class Profiler {
-  constructor(){ this.spans = new Map(); }
-  time(label){ this.spans.set(label, performance.now()); }
-  timeEnd(label){ const t0 = this.spans.get(label); return t0 ? (performance.now() - t0) : 0; }
-}
